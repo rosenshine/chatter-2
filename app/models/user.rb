54d17f15a9928932d22913ba8da9ed43dae5132c
Sarah <srosenshine@gmail.com>
2018-02-14 15:22:39 -0500

    first migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_many :messages
-  has_many :conversations through: :messages
+  has_many :conversations, through: :messages
 end
--- db/migrate/20180214201425_create_messages.rb
+++ db/migrate/20180214201425_create_messages.rb
@@ -1,4 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
+    create_table :messages do |t|
+      t.text :message
+      t.integer :conversation_id
+      t.integer :user_id
+
+      t.timestamps
+    end
   end
 end
