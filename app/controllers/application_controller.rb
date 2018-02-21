0cf0e5b6379d6d85b49f346b0192aa0f3418f364
Sarah <srosenshine@gmail.com>
2018-02-20 21:16:49 -0500

    works w/ bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,36 +5,39 @@ require 'pry'
 
 set :server, 'thin'
 set :sockets, []
+
 class ApplicationController < Sinatra::Base
 
   use Rack::Session::Cookie, :key => 'rack.session',
-                             :path => '/',
-                             :secret => 'your_secret'
-    set :views, 'app/views'
-    set :server, 'thin'
-    set :sockets, []
+  :path => '/',
+  :secret => 'your_secret'
+  set :views, 'app/views'
+  set :server, 'thin'
+  set :sockets, []
 
-    get '/' do
-      @messages = Message.all.reverse
-      if !request.websocket?
-        erb :index
-      else
-        request.websocket do |ws|
-          ws.onopen do
-            settings.sockets << ws
-          end
-          ws.onmessage do |msg|
-            EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-          end
-          ws.onclose do
-            warn("websocket closed")
-            settings.sockets.delete(ws)
-          end
+  get '/' do
+
+    @messages = Message.all.reverse
+
+    if !request.websocket?
+      erb :index
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          settings.sockets << ws
+        end
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
         end
       end
     end
+  end
 
-    post '/' do
-      @message = Message.create(input: params[:input])
-    end
+  post '/' do
+    @message = Message.create(input: params[:input])
+  end
 end
